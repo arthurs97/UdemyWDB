ae4145bb6160b927c4db6982fb34b28f7a1ba455
Arthur <arshi@umich.edu>
2017-07-04 22:46:37 -0400

    color game

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,3 @@
-var squares = document.getElementsByClassName("square");
-
 function randomColor() {
     var colorString = "rgb(";
     colorString += Math.floor(Math.random() * 256) + ", ";
@@ -9,30 +7,47 @@ function randomColor() {
     return colorString;
 }
 
-var winningIndex = Math.floor(Math.random() * 6);
-var winningColor = "";
-
-for (let i = 0; i < squares.length; ++i) {
-    squares[i].style.backgroundColor = randomColor();
-    if (i === winningIndex) {
-        squares[i].classList.add("winner");
-        winningColor = squares[i].style.backgroundColor;
-    } else {
-        squares[i].classList.add("loser");
-    }
+function correct() {
+    console.log("That's correct!");
 }
 
-document.querySelector(".winner").addEventListener("click", function() {
-    console.log("That's correct!");
+function incorrect() {
+    console.log("That's incorrect!");
+}
+
+document.querySelector("#replay").addEventListener("click", function() {
+    console.clear();
+    generate();
 });
 
-var losers = document.querySelectorAll(".loser");
+function generate() {
+    var winningIndex = Math.floor(Math.random() * 6);
+    var winningColor = "";
 
-for (let i = 0; i < losers.length; ++i ) {
-    losers[i].addEventListener("click", function() {
-        console.log("That's incorrect!");
-    });
+    var squares = document.getElementsByClassName("square");
+
+    for (let i = 0; i < squares.length; ++i) {
+        squares[i].removeEventListener("click", correct);
+        squares[i].removeEventListener("click", incorrect);
+        squares[i].classList.remove("winner");
+        squares[i].classList.remove("loser");
+
+        squares[i].style.backgroundColor = randomColor();
+        if (i === winningIndex) {
+            squares[i].classList.add("winner");
+            winningColor = squares[i].style.backgroundColor;
+        } else {
+            squares[i].classList.add("loser");
+        }
+    }
+
+    var prompt = document.querySelector("p");
+    prompt.innerHTML = "Which square is " + winningColor + "?";
+
+    document.querySelector(".winner").addEventListener("click", correct);
+    var losers = document.querySelectorAll(".loser");
+
+    for (let i = 0; i < losers.length; ++i ) {
+        losers[i].addEventListener("click", incorrect);
+    }
 }
-
-var prompt = document.querySelector("p");
-prompt.innerHTML = "Which square is " + winningColor + "?";
